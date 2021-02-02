98b5bb6f72501cdc28dc3dbd642689c29f3f4bcd
Kristiyan Krastev <k.krastev.1998@abv.bg>
2021-02-02 23:47:55 +0200

    init

---
+++ Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour
+{
+    public Player player;
+    public float timeOffset = 2.5f;
+    public bool canMoveY = false;
+
+    public Vector3 posOffset;
+
+    private void FixedUpdate()
+    {
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = player.transform.position;
+        
+        if(player.FacingDirection == Player.Direction.RIGHT)
+        {
+            endPosition.x += posOffset.x;
+        }
+
+        if(player.FacingDirection == Player.Direction.LEFT)
+        {
+            endPosition.x -= posOffset.x;
+        }
+
+        if (!canMoveY)
+        {
+            endPosition.y = 0;
+        }
+        endPosition.z = -10;
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, Time.fixedDeltaTime * timeOffset);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    public float jumpForce;
+    public float speed;
+
+    private Vector3 velocity = Vector3.zero;
+    public bool isGrounded;
+    public GameObject groundCheck;
+
+    public enum Direction
+    {
+        RIGHT,
+        LEFT
+    }
+
+    private Direction direction = Direction.RIGHT;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            FacingDirection = Direction.RIGHT;
+            Vector3 targetVelocity = new Vector2(speed, rb2d.velocity.y);
+
+            rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, Time.fixedDeltaTime * 0.05f);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            FacingDirection = Direction.LEFT;
+            Vector3 targetVelocity = new Vector2(-speed, rb2d.velocity.y);
+
+            rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, Time.fixedDeltaTime * 0.05f);
+        }
+
+        if (Input.GetButtonDown("Jump") || Input.GetButton("Jump"))
+        {
+            if(isGrounded) {
+                isGrounded = false;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            }
+        }
+    }
+
+    public Direction FacingDirection
+    {
+        get
+        {
+            return direction;
+        }
+
+        set
+        {
+            direction = value;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
